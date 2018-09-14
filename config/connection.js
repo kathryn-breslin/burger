4bf32103f0635622a320323e61ece733f8931826
kathryn-breslin <kathryn.l.breslin@gmail.com>
2018-09-14 18:05:54 -0500

    Heroku connection attempt

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,32 @@
 var mysql = require("mysql2");
+var connection;
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306, 
+//     user: "root",
+//     password: null,
+//     database: "burgers_db"
+// });
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306, 
-    user: "root",
-    password: null,
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306, 
+        user: "root",
+        password: null,
+        database: "burgers_db"
+    });
+}
+connection.connect();
 
-connection.connect(function(err) {
-    if (err) {
-        console.log("Error connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as ID " + connection.threadId);
-});
+// connection.connect(function(err) {
+//     if (err) {
+//         console.log("Error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("Connected as ID " + connection.threadId);
+// });
 
 module.exports = connection;
